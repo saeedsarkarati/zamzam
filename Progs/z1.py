0bbf9dc05a6ba570bbb5768c57236e7440e437b5
saeedsarkarati <saeedsarkarati@gmail.com>
2022-01-02 19:28:16 +0330

    c3

---
+++ Progs/z1.py
@@ -0,0 +1,11 @@
+class person:
+	def __init__(self):
+		self.name = "w"
+		self.surname = "q"
+		self.idnum = "0441024376"
+class waterman(person):
+	def __init__(self):
+		self.account = 12
+class wellman(waterman)
+	
+
