43c0e3b44543000d24979bdcf00f21383bea49d6
saeedsarkarati <saeedsarkarati@gmail.com>
2022-01-09 19:56:36 +0330

    c4

--- Progs/z1.py
+++ Progs/z1.py
@@ -1,11 +1,20 @@
-class person:
+class Twell:
+	def __init__(self):
+		usecap = 1000
+	def sellcap():
+		return usecap * 0.1
+class Tperson:
 	def __init__(self):
 		self.name = "w"
 		self.surname = "q"
 		self.idnum = "0441024376"
-class waterman(person):
+class Twaterman(person):
 	def __init__(self):
 		self.account = 12
-class wellman(waterman)
+class wellman(waterman):
+	def __init__(self):
+		self.well = None
+# ~ از نوع twell تعریف شده است.
+class 
 	
 
