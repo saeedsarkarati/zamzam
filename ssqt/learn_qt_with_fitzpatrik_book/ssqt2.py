9e45e683ff6c9fb9caf5e2c438bcb883ddc193f6
saeedsarkarati <saeedsarkarati@gmail.com>
2022-03-07 18:06:02 +0330

    Create ssqt2.py
    c6

---
+++ ssqt/learn_qt_with_fitzpatrik_book/ssqt2.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle("My App")
+		button = QPushButton("class")
+		self.setFixedSize(QSize(400, 300))
+		# Set the central widget of the Window.
+		self.setCentralWidget(button)
+app = QApplication(sys.argv)
+window = MainWindow()
+window.setMaximumSize(QSize(400, 400))
+window.setWindowTitle("class 2")
+window.show()
+ww = MainWindow()
+ww.show()
+w = QMainWindow()
+w.setWindowTitle("sssss")
+w.setMinimumSize(QSize(400, 300))
+button = QPushButton("ssss")
+w.setCentralWidget(button)
+w.show()
+app.exec()
